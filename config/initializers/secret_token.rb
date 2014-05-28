8031c571125002880c7e195074ffd464a12864af
alessio <alex92_v@hotmail.it>
2014-05-28 13:58:38 +0200

    Autenticazione login e logout

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Autenticazione::Application.config.secret_key_base = '15c4bfa6d9430b5cf6e1d6ebbd93aa308975440981d7fe12e575921a3f2edc3b9d55dd57f1cf4758a37eb81ed12dd1a7a6da61f80148aee140ddd65f7dd1384e'
