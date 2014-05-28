26bf773a4d606e9b406a40fda8da5a9e36a1bb28
alessio <alex92_v@hotmail.it>
2014-05-28 14:13:41 +0200

    Autenticazione com cambiamento password

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,5 +7,13 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
 
 
+  before_filter :configure_permitted_parameters, if: :devise_controller?
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:account_update) { |u|
+      u.permit(:password, :password_confirmation, :current_password)
+    }
+  end
+
 
 end
